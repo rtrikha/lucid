c12b188c0ea17c091c7d59175b1e68e3c8d332cb
rtikha <raunakstrikha@gmail.com>
2020-06-02 11:45:19 +0530

    adding styling of audio bars

--- audio.js
+++ audio.js
@@ -10,10 +10,10 @@ function initSlider(id, eventValue) {
 	});
 }
 
-initSlider('#wave-volume',setVolumeWave);
-initSlider('#rain-volume',setVolumeRain);
-initSlider('#thunder-volume',setVolumeThunder);
-
+initSlider('#wave-volume', setVolumeWave);
+initSlider('#rain-volume', setVolumeRain);
+initSlider('#thunder-volume', setVolumeThunder);
+initSlider('#birds-volume', setVolumeBirds);
 
 var waveSound = document.createElement('audio');
 $('#wave-player').append(waveSound);
@@ -24,13 +24,18 @@ $('#rain-player').append(rainSound);
 var thunderSound = document.createElement('audio');
 $('#thunder-player').append(thunderSound);
 
+var birdsSound = document.createElement('audio');
+$('#birds-player').append(birdsSound);
+
 waveSound.id = 'waveSound';
 rainSound.id = 'rainSound';
 thunderSound.id = 'thunderSound';
+birdsSound.id = 'birdsSound';
 
 playAudio(waveSound, '/resources/audio/waves.mp3', 0.5, setVolumeWave);
 playAudio(rainSound, '/resources/audio/rain.mp3', 0.5, setVolumeRain);
 playAudio(thunderSound, '/resources/audio/thunder.mp3', 0.5, setVolumeRain);
+playAudio(birdsSound, '/resources/audio/birds.mp3', 0.5, setVolumeBirds);
 
 function playAudio(id, fileName, myVolume, volumeControl) {
 	id.src = fileName;
@@ -52,4 +57,9 @@ function setVolumeRain(myVolume) {
 function setVolumeThunder(myVolume) {
 	var thunderSound = document.getElementById('thunderSound');
 	thunderSound.volume = myVolume;
+}
+
+function setVolumeBirds(myVolume) {
+	var birdsSound = document.getElementById('birdsSound');
+	birdsSound.volume = myVolume;
 }
