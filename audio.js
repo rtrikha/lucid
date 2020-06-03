b9f546b3af8a189bdafe2f0b1d3de97cad645fe2
rtikha <raunakstrikha@gmail.com>
2020-06-03 13:35:44 +0530

    adding colours of breathe

--- audio.js
+++ audio.js
@@ -4,16 +4,18 @@ function initSlider(id, eventValue) {
 		max: 100,
 		value: 50,
 		range: 'min',
+		orientation:"vertical",
+		reversed : false,
 		slide: function (event, ui) {
 			eventValue(ui.value / 100);
 		},
 	});
 }
 
-function setterAudio(path,path2) {
+function setterAudio(path, path2) {
 	var x = document.createElement('audio');
 	$(path).append(x);
-	return x.id=path2;
+	return (x.id = path2);
 }
 
 initSlider('#wave-volume', setVolumeWave);
@@ -21,15 +23,17 @@ initSlider('#rain-volume', setVolumeRain);
 initSlider('#thunder-volume', setVolumeThunder);
 initSlider('#birds-volume', setVolumeBirds);
 
-setterAudio('#wave-player','waveSound');
+setterAudio('#wave-player', 'waveSound');
 setterAudio('#rain-player', 'rainSound');
-setterAudio('#thunder-player','thunderSound');
-setterAudio('#birds-player','birdsSound');
+setterAudio('#thunder-player', 'thunderSound');
+setterAudio('#birds-player', 'birdsSound');
 
-// playAudio(waveSound, '/resources/audio/waves.mp3', 0.5, setVolumeWave);
-// playAudio(rainSound, '/resources/audio/rain.mp3', 0.5, setVolumeRain);
-// playAudio(thunderSound, '/resources/audio/thunder.mp3', 0.5, setVolumeRain);
-// playAudio(birdsSound, '/resources/audio/birds.mp3', 0.5, setVolumeBirds);
+function playAllAudio() {
+	playAudio(waveSound, '/resources/audio/waves.mp3', 0.5, setVolumeWave);
+	playAudio(rainSound, '/resources/audio/rain.mp3', 0.5, setVolumeRain);
+	playAudio(thunderSound, '/resources/audio/thunder.mp3', 0.5, setVolumeRain);
+	playAudio(birdsSound, '/resources/audio/birds.mp3', 0.5, setVolumeBirds);
+}
 
 function playAudio(id, fileName, myVolume, volumeControl) {
 	id.src = fileName;
--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+// var timeout;
+// document.onmousemove = function () {
+// 	clearTimeout(timeout);
+// 	timeout = setTimeout(function () {
+// 		//document.getElementById('header-nav').style.opacity = 0.2;
+// 		//document.getElementById('header-nav').style.transition = 'opacity 0.3s';
+// 	}, 2000);
+// };
+
+function drawCircle(circleID, color) {
+	document.getElementById(circleID).style.background = color;
+}
+function changeColorofBreathe(oddColor,evenColor,time){
+    setTimeout(function () {
+        drawCircle('c1', oddColor);
+        drawCircle('c2', evenColor);
+        drawCircle('c3', oddColor);
+        drawCircle('c4', evenColor);
+        drawCircle('c5', oddColor);
+        drawCircle('c6', evenColor);
+    }, time);
+}
+
+changeColorofBreathe('#4C4C4E','#282828',0);
+changeColorofBreathe('#50635F','#303F40',12000);
+changeColorofBreathe('#53796E','#385658',24000);
+changeColorofBreathe('#589080','#416D70',36000);
+changeColorofBreathe('#5CA791','#498488',48000);
+changeColorofBreathe('#61BEA2','#529CA0',60000);
 
 
 
